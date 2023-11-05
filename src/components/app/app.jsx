57398541c126de0e259e625426762ffc26e1e73c
tatnik <ptn48@yandex.ru>
2023-11-05 20:17:18 +0300

    add routing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,32 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
 import ProductPage from "../../pages/ProductPage";
 import { products } from "../../mocks/mock";
+import Catalog from "../catalog/catalog";
+import Layout from "../layout/layout";
 
+function ProductOr404({ products }) {
+  const { code } = useParams();
+  const product = products.find((product) => product.code.toString() === code);
+  return product ? (
+    <ProductPage product={product} />
+  ) : (
+    <h1>404 страница не найдена</h1>
+  );
+}
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={<ProductPage product={products[0]} showInfoInAccordion />}
-        />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Catalog products={products} />} />
+          <Route path="product">
+            <Route
+              path=":code"
+              element={<ProductOr404 products={products} />}
+            />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
